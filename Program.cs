ab4d970d4e64d513ee35bc6a92e8c2297eb72def
Alexander Baranski <m.alex.baranski@gmail.com>
2020-11-05 09:58:06 +0100

    WorkflowComment

--- Program.cs
+++ Program.cs
@@ -16,5 +16,16 @@ namespace Github
             Console.WriteLine("Test ausgabe, awesome");
             // fertig
         }
+
+
+        /* 1. Repository aus Projekt erstellen
+         * 2. Branch erzeugen
+         * 3. Änderungen erstellen
+         * 4. Änderungen Comitten
+         * 5. Wenn Branch abgeschlossen (definitionssache) dann Pull Request
+         * 6. Projekt verantwortlicher prüft Pull Request
+         * 7. Annahme oder Rückgabe des Pull Request
+         * 8. Branch wird in GitHub gemerged
+         * 9. Done!
     }
 }
